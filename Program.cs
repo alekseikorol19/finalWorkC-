e5ef2a18fc778c48bee4acbdafb2f9a8532d4779
Aleksei Korol <joker.enot@gmail.com>
2024-02-22 13:12:51 +1000

    Did task 3

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿void ReversedArray(int[] array, int j) {
+    if (j == 0) {
+        Console.Write(array[j]);
+        return;
+    } else {
+        Console.Write(array[j] + " ");
+        ReversedArray(array,j-1);
+    }
+}
+ReversedArray([1,2,3,4,5,6],5);
