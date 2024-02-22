af20f4aaba7c327bd61c3549e7e79ac77dff2a8f
Aleksei Korol <139727725+alekseikorol19@users.noreply.github.com>
2024-02-22 13:32:27 +1000

    Add files via upload

---
+++ VCS_Final_Csharp/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿void ShowNumbers (int start, int end) {
+    if (start == end) {
+        Console.Write(start);
+        return;
+    }
+    Console.Write(start + " ");
+    ShowNumbers(start + 1,end);
+    
+}
+ShowNumbers(1,5);
